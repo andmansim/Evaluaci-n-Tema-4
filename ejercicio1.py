82add933292d4ec1ba1d6f4de9cd716c1747ddd7
andmansim <91722324+andmansim@users.noreply.github.com>
2022-11-19 16:54:44 +0100

    enunciado

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+'''
+ crear un árbol de Huffman a partir de la siguiente tabla:
+
+Símbolo Frecuencia
+
+A 0.2
+F 0.17
+1 0.13
+3 0.21
+0 0.05
+M 0.09
+T 0.15
+
+b.         desarrollar las funcionas para comprimir y descomprimir un mensaje.
+'''
