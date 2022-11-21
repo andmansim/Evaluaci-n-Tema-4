6dbbb12b53b5e5f4e2cdfc1b601fc1b6a6adeb5a
andmansim <91722324+andmansim@users.noreply.github.com>
2022-11-21 12:04:04 +0100

    código inicial grafo

---
+++ ejercicio3.py
@@ -0,0 +1,79 @@
+'''
+Ejercicio 3
+
+Se requiere implementar un grafo para almacenar las siete maravillas arquitectónicas modernas y naturales del mundo, para lo cual se deben tener en cuenta las siguientes actividades:
+a.         de cada una de las maravillas se conoce su nombre, país de ubicación (puede ser más de uno en las naturales) y tipo (natural o arquitectónica);
+b.         cada una debe estar relacionada con las otras seis de su tipo, para lo que se debe almacenar la distancia que las separa;
+c.         hallar el árbol de expansión mínimo de cada tipo de las maravillas;
+d.         determinar si existen países que dispongan de maravillas arquitectónicas y naturales;
+e.         determinar si algún país tiene más de una maravilla del mismo tipo;
+f.         deberá utilizar un grafo no dirigido.
+'''
+
+class nodoArista(object):
+    '''Clase nodo vértice'''
+    def __init__(self, info, destino):
+        '''
+        Crea un nodo arista con la información cargada
+        '''
+        self.info = info
+        self.destino = destino
+        self.sig = None
+    
+class nodoVertice(object):
+    '''
+    Clase nodo vértice
+    '''
+    def __init__(self, info):
+        '''
+        Crea un nodo vértice con la información cargada
+        '''
+        self.info = info
+        self.sig = None
+        self.visitado = False
+        self.adyacentes = Arista()
+
+class Grafo(object):
+    '''
+    Clase grafo implementación lista de listas de adyacencia
+    '''
+    def __init__(self, dirigido = True):
+        '''
+        Crea un gráfo vacío
+        '''
+        self.inicio = None
+        self.dirigido = dirigido
+        self.tamanio = 0
+        
+class Arista(object):
+    '''
+    Clase lista de aristas implenentación sobre lista
+    '''
+    def __init__(self):
+        '''
+        Crea una lista de aristas vacía
+        '''
+        self.inicio = None
+        self.tamanio = 0
+        
+    def insertar_vertice(grafo, dato):
+        '''
+        Insertar un vértice grafo
+        '''
+        nodo = nodoVertice(dato)
+        if grafo.inicio is None or grafo.inicio.info > nodo.info:
+            nodo.sig = grafo.inicio
+            grafo.inicio = nodo
+        else:
+            ant = grafo.inicio
+            act = grafo.inicio.sig
+            while act is not None and act.info < nodo.info:
+                ant = act
+                act = act.sig
+            nodo.sig = act
+            ant.sig = nodo
+        grafo.tamanio += 1
+    
+    def insertar_arista(grafo, dato, origen, destino):
+        
+                
